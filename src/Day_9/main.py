44b363445da25d622491af43f32015ac8095cf70
Ryan Salter <89457004+retlasnayr@users.noreply.github.com>
2023-12-09 09:32:32 +0000

    Complete part 1

---
+++ src/Day_9/main.py
@@ -0,0 +1,32 @@
+import utils
+Day = utils.AdventOfCodeDay(9)
+Part_1 = utils.AdventOfCodePart(Day, 1)
+
+class Solution(utils.AdventOfCodeSolution):
+    def __init__(self):
+        utils.AdventOfCodeSolution.__init__(self)
+
+    def main(self, input_data):
+        total = 0
+        for row in input_data:
+            row_list = self.parse_row(row)
+            total += self.process_sequence(row_list)
+        return total
+    
+    def parse_row(self, row):
+        return list(map(int, row.split()))
+
+    def process_sequence(self, seq):
+        if all(x == 0 for x in seq):
+            return 0
+        return seq[-1] + self.process_sequence(self.calc_diffs(seq))
+    
+    def calc_diffs(self, seq):
+        pairs = zip(seq, seq[1:])
+        return [x[1] - x[0] for x in pairs]
+
+Part_1.set_solution(Solution)
+
+if __name__ == "__main__":
+    Part_1.run_display_all()
+    
