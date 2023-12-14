ab0f4e4d4ac9ba01cebe8c821b5c8278fbd04d33
Ryan Salter <89457004+retlasnayr@users.noreply.github.com>
2023-12-14 13:53:40 +0000

    Solve part 2 bug Using lists ended up creating a mutable history Using copy.deepcopy was a quick solution

--- src/Day_14/main.py
+++ src/Day_14/main.py
@@ -1,6 +1,17 @@
-import utils
+import utils, copy
 Day = utils.AdventOfCodeDay(14)
 Part_1 = utils.AdventOfCodePart(Day, 1)
+
+class DataGrid:
+    def __init__(self, data):
+        self.data = data
+    
+    def __hash__(self) -> int:
+        return(hash(tuple(tuple(x) for x in self.data)))
+    
+    def __eq__(self, other):
+        return self.data == other.data
+
 
 class Solution(utils.AdventOfCodeSolution):
     def __init__(self):
@@ -40,6 +51,69 @@ class Solution(utils.AdventOfCodeSolution):
         return len(self.data) - row 
 Part_1.set_solution(Solution)
 
+Part_2 = utils.AdventOfCodePart(Day, 2)
+
+class Solution2(Solution):
+    def __init__(self):
+        super().__init__()
+
+    def main(self, input_data):
+        self.store_input(input_data)
+        self.store_input(self.rotate_data())
+        self.store_input(self.rotate_data())
+        self.store_input(self.rotate_data())
+        self.rotate_data()
+        # print(self.display_grid(self.data))
+        prev = [copy.deepcopy(self.data)]
+        cycles_to_run = 1000000000
+        for _ in range(cycles_to_run):
+            prev.append(copy.deepcopy(self.data))
+            self.run_cycle()
+            # print(self.display_grid(self.data))
+            if self.data in prev:
+                repeat_length = len(prev) - prev.index(self.data)
+                cycles_remaining = cycles_to_run - len(prev)
+                remainder = cycles_remaining % repeat_length
+                break
+        for _ in range(remainder + 1):
+            self.run_cycle()
+        return self.total_load()
+
+        # print(self.data)
+        # print(self.rotate_data())
+        # self.roll_north()
+        # return self.total_load()
+
+    def run_cycle(self):
+        for _ in range(4):
+            self.roll_north()
+            self.rotate_data()
+
+    def rotate_data(self):
+        new_data = [[] for _ in self.data[0]]
+        for row in self.data[::-1]:
+            for index, col in enumerate(row):
+                new_data[index].append(col)
+        self.data = new_data
+        return new_data
+    
+    @staticmethod
+    def display_grid(grid):
+        return "\n".join("".join(row) for row in grid)
+
+    @staticmethod
+    def grid_diff(g1, g2):
+        for row, (r1, r2) in enumerate(zip(g1, g2)):
+            for col, (c1, c2) in enumerate(zip(r1, r2)):
+                if c1 != c2:
+                    print(row, col, c1, c2)
+
+    def rock_load(self, row):
+        return len(self.data) - row - 1  # ignore the extra row of #s at the bottom
+    
+Part_2.set_solution(Solution2)
+
 if __name__ == "__main__":
     Part_1.run_display_all()
+    Part_2.run_display_all()
     
